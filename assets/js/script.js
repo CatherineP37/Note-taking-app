cbbd6720645cb9baed064f2691d9f964275bb623
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-04-30 12:11:48 +0100

    Add displayList() function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,21 @@ let notes = []
 let noteInput = document.getElementById('note_input');
 let noteList = document.getElementById('note_list');
 
+function displayList() {
+  taskList.innerHTML = null;
+  for(const[idx, item] of Object.entries(tasks)) {
+    const container = document.createElement('div');    
+    taskList.appendChild(container);
+  }
+}
+
 function addNote() {
   let noteInputValue = noteInput.value;  
   notes.push(noteInputValue);
+  displayList();
   noteInput.value = ''; 
 }
 
-console.log(notes);
+document.addEventListener("DOMContentLoaded", displayList)
 
 
