18ccc1eabe327cf36350fa40ad7c36b58e061063
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-05-05 08:17:12 +0100

    Edit displayList() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ function displayList() {
     const editButton = document.createElement('button');
     editButton.className = 'edit-button';
     editButton.textContent = 'EDIT';
-    editButton.onclick = () => openModal(i);
+    editButton.onclick = () => editNote(i);
     const deleteButton = document.createElement('button');
     deleteButton.className = 'delete-button';
     deleteButton.textContent = 'DELETE';
